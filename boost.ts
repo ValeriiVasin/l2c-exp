5263e32f46755bc8b893fc49f250b20a68d7d7cc
Valerii Vasin <vvasin@groupon.com>
2018-11-08 23:36:25 +0100

    restructure table; apply purecss; add id to every boost

--- boost.ts
+++ boost.ts
@@ -18,12 +18,54 @@ export enum BoostGroup {
   Q = 'Q'
 }
 
-interface Boost {
+export enum BoostId {
+  FortuneOne = 'fortune1',
+  FortuneTwo = 'fortune2',
+  FortuneThree = 'fortune3',
+  UnityOne = 'unity1',
+  UnityTwo = 'unity2',
+  UnityThree = 'unity3',
+  UnityFour = 'unity4',
+  RuneOne = 'rune100',
+  RuneTwo = 'rune50',
+  Iren = 'iren',
+  Ragu = 'ragu',
+  Dandy = 'dandy',
+  Cat = 'cat',
+  Tower = 'tower',
+  Matryoshka = 'matryoshka',
+  Scroll = 'scroll',
+  ScrollMedium = 'scroll-medium',
+  ScrollHigh = 'scroll-high',
+  ScrollCastle = 'scroll-castle',
+  ScrollCastleMedium = 'scroll-castle-medium',
+  ScrollOly = 'scroll-oly',
+  Storm = 'storm',
+  StormHeavy = 'storm-heavy',
+  Savior = 'savior',
+  Pie = 'pie',
+  PieHigh = 'pie-high',
+  Pirate = 'pirate',
+  Dragon = 'dragon',
+  OlyHero = 'oly-hero',
+  Tears = 'tears',
+  Hat = 'hat',
+  BlessingLight = 'blessing-light',
+  RuneFrequenter = 'rune-frequenter',
+  AmuletHero = 'amulet-hero',
+  ClanExuberanceOne = 'clan-exuberance-one',
+  ClanExuberanceTwo = 'clan-exuberance-two',
+  ClanExuberanceThree = 'clan-exuberance-three',
+  ClanExuberanceFour = 'clan-exuberance-four'
+}
+
+export interface Boost {
   group: BoostGroup;
   exp: number;
   sp: number;
   name: string;
   image: string;
+  id: BoostId;
 }
 
 const TDB_IMAGE = 'https://via.placeholder.com/32x32';
@@ -34,21 +76,24 @@ export const boosts: Boost[] = [
     exp: 3,
     sp: 0,
     name: 'Удача Клана - Ур.1',
-    image: 'https://l2central.info/c/images/b/b5/Skill_392.jpg'
+    image: 'https://l2central.info/c/images/b/b5/Skill_392.jpg',
+    id: BoostId.FortuneOne
   },
   {
     group: BoostGroup.A,
     exp: 6,
     sp: 0,
     name: 'Удача Клана - Ур.2',
-    image: 'https://l2central.info/c/images/b/b5/Skill_392.jpg'
+    image: 'https://l2central.info/c/images/b/b5/Skill_392.jpg',
+    id: BoostId.FortuneTwo
   },
   {
     group: BoostGroup.A,
     exp: 10,
     sp: 0,
     name: 'Удача Клана - Ур.3',
-    image: 'https://l2central.info/c/images/b/b5/Skill_392.jpg'
+    image: 'https://l2central.info/c/images/b/b5/Skill_392.jpg',
+    id: BoostId.FortuneThree
   },
 
   {
@@ -56,7 +101,8 @@ export const boosts: Boost[] = [
     exp: 3,
     sp: 3,
     name: 'Единство Клана - Ур.1',
-    image: 'https://l2central.info/c/images/2/2f/Skill_55168_1.jpg'
+    image: 'https://l2central.info/c/images/2/2f/Skill_55168_1.jpg',
+    id: BoostId.UnityOne
   },
 
   {
@@ -64,21 +110,24 @@ export const boosts: Boost[] = [
     exp: 5,
     sp: 5,
     name: 'Единство Клана - Ур.2',
-    image: 'https://l2central.info/c/images/e/e6/Skill_55169_1.jpg'
+    image: 'https://l2central.info/c/images/e/e6/Skill_55169_1.jpg',
+    id: BoostId.UnityTwo
   },
   {
     group: BoostGroup.B,
     exp: 7,
     sp: 7,
     name: 'Единство Клана - Ур.3',
-    image: 'https://l2central.info/c/images/7/78/Skill_55170_1.jpg'
+    image: 'https://l2central.info/c/images/7/78/Skill_55170_1.jpg',
+    id: BoostId.UnityThree
   },
   {
     group: BoostGroup.B,
     exp: 10,
     sp: 10,
     name: 'Единство Клана - Ур.4',
-    image: 'https://l2central.info/c/images/2/21/Skill_55171_1.jpg'
+    image: 'https://l2central.info/c/images/2/21/Skill_55171_1.jpg',
+    id: BoostId.UnityFour
   },
 
   {
@@ -86,7 +135,8 @@ export const boosts: Boost[] = [
     exp: 100,
     sp: 100,
     name: 'Руна Опыта - 100% (2 часа)',
-    image: 'https://l2central.info/c/images/1/1c/Item_70094.jpg'
+    image: 'https://l2central.info/c/images/1/1c/Item_70094.jpg',
+    id: BoostId.RuneOne
   },
 
   {
@@ -94,7 +144,8 @@ export const boosts: Boost[] = [
     exp: 50,
     sp: 50,
     name: 'Руна Опыта и SP 50%',
-    image: 'https://l2central.info/c/images/1/1c/Item_70094.jpg'
+    image: 'https://l2central.info/c/images/1/1c/Item_70094.jpg',
+    id: BoostId.RuneTwo
   },
 
   {
@@ -102,7 +153,8 @@ export const boosts: Boost[] = [
     exp: 50,
     sp: 50,
     name: 'Песочные Часы Ирен',
-    image: 'https://l2central.info/c/images/4/49/Item_70263.jpg'
+    image: 'https://l2central.info/c/images/4/49/Item_70263.jpg',
+    id: BoostId.Iren
   },
 
   {
@@ -110,14 +162,16 @@ export const boosts: Boost[] = [
     exp: 10,
     sp: 10,
     name: 'Рыбное Рагу - Благодарность Гильдии',
-    image: 'https://l2central.info/c/images/c/c7/Item_49501.jpg'
+    image: 'https://l2central.info/c/images/c/c7/Item_49501.jpg',
+    id: BoostId.Ragu
   },
   {
     group: BoostGroup.F,
     exp: 100,
     sp: 100,
     name: 'Счастливый Мяч Данди',
-    image: 'http://l2on.net//img/icons/g_ev_homerunball.png'
+    image: 'http://l2on.net//img/icons/g_ev_homerunball.png',
+    id: BoostId.Dandy
   },
 
   {
@@ -125,21 +179,24 @@ export const boosts: Boost[] = [
     exp: 20,
     sp: 20,
     name: 'Благословение Кота-Ангела / Энергия Кота-Ангела',
-    image: 'https://l2central.info/c/images/1/12/Item_35669.jpg'
+    image: 'https://l2central.info/c/images/1/12/Item_35669.jpg',
+    id: BoostId.Cat
   },
   {
     group: BoostGroup.G,
     exp: 30,
     sp: 30,
     name: 'Свиток Благословения Сторожевой Башни',
-    image: 'https://l2central.info/c/images/0/09/Item_49765.jpg'
+    image: 'https://l2central.info/c/images/0/09/Item_49765.jpg',
+    id: BoostId.Tower
   },
   {
     group: BoostGroup.G,
     exp: 30,
     sp: 30,
     name: 'Благословение Матрешки',
-    image: TDB_IMAGE
+    image: TDB_IMAGE,
+    id: BoostId.Matryoshka
   },
 
   {
@@ -147,35 +204,40 @@ export const boosts: Boost[] = [
     exp: 30,
     sp: 30,
     name: 'Свиток Опыта/SP - Обычный',
-    image: 'https://l2central.info/c/images/1/1a/Item_29648.jpg'
+    image: 'https://l2central.info/c/images/1/1a/Item_29648.jpg',
+    id: BoostId.Scroll
   },
   {
     group: BoostGroup.H,
     exp: 40,
     sp: 40,
     name: 'Свиток Опыта/SP - Среднее Качество',
-    image: 'https://l2central.info/c/images/b/bc/Item_29649.jpg'
+    image: 'https://l2central.info/c/images/b/bc/Item_29649.jpg',
+    id: BoostId.ScrollMedium
   },
   {
     group: BoostGroup.H,
     exp: 100,
     sp: 100,
     name: 'Свиток Опыта/SP - Высокое Качество',
-    image: 'https://l2central.info/c/images/8/86/Item_29650.jpg'
+    image: 'https://l2central.info/c/images/8/86/Item_29650.jpg',
+    id: BoostId.ScrollHigh
   },
   {
     group: BoostGroup.H,
     exp: 35,
     sp: 35,
     name: 'Свиток Опыта/SP Замка - Обычный',
-    image: 'https://l2central.info/c/images/b/b4/Item_29669.jpg'
+    image: 'https://l2central.info/c/images/b/b4/Item_29669.jpg',
+    id: BoostId.ScrollCastle
   },
   {
     group: BoostGroup.H,
     exp: 45,
     sp: 45,
     name: 'Свиток Опыта/SP Замка - Среднего качества',
-    image: 'https://l2central.info/c/images/f/f2/Item_29670.jpg'
+    image: 'https://l2central.info/c/images/f/f2/Item_29670.jpg',
+    id: BoostId.ScrollCastleMedium
   },
 
   {
@@ -183,28 +245,32 @@ export const boosts: Boost[] = [
     exp: 20,
     sp: 20,
     name: 'Свиток Опыта/SP (Олимпиада)',
-    image: 'https://l2central.info/c/images/a/ac/Item_29565.jpg'
+    image: 'https://l2central.info/c/images/a/ac/Item_29565.jpg',
+    id: BoostId.ScrollOly
   },
   {
     group: BoostGroup.I,
     exp: 50,
     sp: 50,
     name: 'Свиток Призыва Шторма',
-    image: 'https://l2central.info/c/images/0/08/Item_49420.jpg'
+    image: 'https://l2central.info/c/images/0/08/Item_49420.jpg',
+    id: BoostId.Storm
   },
   {
     group: BoostGroup.I,
     exp: 50,
     sp: 50,
     name: 'Свиток Призыва Сильного Шторма',
-    image: 'https://l2central.info/c/images/0/08/Item_49420.jpg'
+    image: 'https://l2central.info/c/images/0/08/Item_49420.jpg',
+    id: BoostId.StormHeavy
   },
   {
     group: BoostGroup.I,
     exp: 50,
     sp: 50,
     name: 'Свиток Роста Спасителя',
-    image: TDB_IMAGE
+    image: TDB_IMAGE,
+    id: BoostId.Savior
   },
 
   {
@@ -212,14 +278,16 @@ export const boosts: Boost[] = [
     exp: 20,
     sp: 20,
     name: 'Рисовый Пирожок Боевого Духа',
-    image: 'https://l2central.info/c/images/0/0f/Item_49080.jpg'
+    image: 'https://l2central.info/c/images/0/0f/Item_49080.jpg',
+    id: BoostId.Pie
   },
   {
     group: BoostGroup.J,
     exp: 30,
     sp: 30,
     name: 'Рисовый Пирожок Пылающего Боевого Духа',
-    image: 'https://l2central.info/c/images/2/23/Item_49081.jpg'
+    image: 'https://l2central.info/c/images/2/23/Item_49081.jpg',
+    id: BoostId.PieHigh
   },
 
   {
@@ -227,22 +295,25 @@ export const boosts: Boost[] = [
     exp: 5,
     sp: 5,
     name: 'Особый Плод Пирата',
-    image: 'https://l2central.info/c/images/8/8f/Item_49518.jpg'
+    image: 'https://l2central.info/c/images/8/8f/Item_49518.jpg',
+    id: BoostId.Pirate
   },
   {
     group: BoostGroup.K,
     exp: 10,
     sp: 10,
     name: 'Особый Плод Дракона',
-    image: 'https://l2central.info/c/images/1/1d/Item_70767.jpg'
+    image: 'https://l2central.info/c/images/1/1d/Item_70767.jpg',
+    id: BoostId.Dragon
   },
-  // TODO: fix Image
+
   {
     group: BoostGroup.K,
     exp: 10,
     sp: 10,
     name: 'Особый Плод Героя Олимпиады',
-    image: TDB_IMAGE
+    image: TDB_IMAGE,
+    id: BoostId.OlyHero
   },
 
   {
@@ -250,7 +321,8 @@ export const boosts: Boost[] = [
     exp: 50,
     sp: 50,
     name: 'Слезы Золотой Русалки',
-    image: 'https://l2central.info/c/images/6/66/Item_49585.jpg'
+    image: 'https://l2central.info/c/images/6/66/Item_49585.jpg',
+    id: BoostId.Tears
   },
 
   {
@@ -258,7 +330,8 @@ export const boosts: Boost[] = [
     exp: 5,
     sp: 5,
     name: 'Всепоглощающая Сила Праздничной Шляпы',
-    image: 'https://l2central.info/c/images/1/1c/Item_70094.jpg'
+    image: 'https://l2central.info/c/images/1/1c/Item_70094.jpg',
+    id: BoostId.Hat
   },
 
   {
@@ -266,7 +339,8 @@ export const boosts: Boost[] = [
     exp: 3,
     sp: 3,
     name: 'Благословение Света',
-    image: TDB_IMAGE
+    image: TDB_IMAGE,
+    id: BoostId.BlessingLight
   },
 
   {
@@ -274,7 +348,8 @@ export const boosts: Boost[] = [
     exp: 10,
     sp: 10,
     name: 'Руна Опыта Завсегдатая',
-    image: 'https://l2central.info/c/images/1/1c/Item_70094.jpg'
+    image: 'https://l2central.info/c/images/1/1c/Item_70094.jpg',
+    id: BoostId.RuneFrequenter
   },
 
   {
@@ -282,7 +357,8 @@ export const boosts: Boost[] = [
     exp: 20,
     sp: 20,
     name: 'Амулет Опыта Героя',
-    image: 'https://l2central.info/c/images/5/53/Item_90836.jpg'
+    image: 'https://l2central.info/c/images/5/53/Item_90836.jpg',
+    id: BoostId.AmuletHero
   },
 
   {
@@ -290,27 +366,51 @@ export const boosts: Boost[] = [
     exp: 5,
     sp: 5,
     name: 'Изобилие Клана - Ур.1',
-    image: 'https://l2central.info/c/images/7/7a/Skill_55887_2.jpg'
+    image: 'https://l2central.info/c/images/7/7a/Skill_55887_2.jpg',
+    id: BoostId.ClanExuberanceOne,
   },
   {
     group: BoostGroup.Q,
     exp: 8,
     sp: 8,
     name: 'Изобилие Клана - Ур.2',
-    image: 'https://l2central.info/c/images/7/7a/Skill_55887_2.jpg'
+    image: 'https://l2central.info/c/images/7/7a/Skill_55887_2.jpg',
+    id: BoostId.ClanExuberanceTwo,
   },
   {
     group: BoostGroup.Q,
     exp: 11,
     sp: 11,
     name: 'Изобилие Клана - Ур.3',
-    image: 'https://l2central.info/c/images/7/7a/Skill_55887_2.jpg'
+    image: 'https://l2central.info/c/images/7/7a/Skill_55887_2.jpg',
+    id: BoostId.ClanExuberanceThree,
   },
   {
     group: BoostGroup.Q,
     exp: 15,
     sp: 15,
     name: 'Изобилие Клана - Ур.4',
-    image: 'https://l2central.info/c/images/7/7a/Skill_55887_2.jpg'
+    image: 'https://l2central.info/c/images/7/7a/Skill_55887_2.jpg',
+    id: BoostId.ClanExuberanceFour,
   }
 ];
+
+export const groups = Object.keys(BoostGroup);
+
+interface GroopedBosts {
+  [key: string]: Boost[];
+}
+
+export const byGroup: GroopedBosts = boosts.reduce(
+  (acc, boost) => {
+    const group: BoostGroup = boost.group;
+
+    if (!acc[group]) {
+      acc[group] = [];
+    }
+
+    acc[group] = [...acc[group], boost];
+    return acc;
+  },
+  {} as GroopedBosts
+);
--- index.tsx
+++ index.tsx
@@ -1,31 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, SFC } from 'react';
 import { render } from 'react-dom';
-import { BoostGroup, boosts } from './boost';
+import { boosts, Boost, BoostGroup, groups, byGroup } from './boost';
+import classNames from 'classnames';
 
-const groups = Object.keys(BoostGroup);
+const Group: SFC<{ group: BoostGroup; boosts: Boost[]; odd: boolean }> = ({
+  group,
+  boosts,
+  odd
+}) => {
+  return (
+    <tbody className={classNames({ ['pure-table-odd']: odd })}>
+      {boosts.map(boost => {
+        return (
+          <tr key={boost.id}>
+            <td>
+              <img src={boost.image} />
+            </td>
+            <td>{boost.name}</td>
+            <td>{`${boost.exp}%`}</td>
+            <td>{`${boost.sp}%`}</td>
+            <td>{group}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+};
 
 const App = () => (
-  <table>
-    <tr>
-      <th>image</th>
-      <th>name</th>
-      <th>EXP</th>
-      <th>SP</th>
-      <th>Group</th>
-    </tr>
-    {boosts.map(boost => {
-      return (
-        <tr>
-          <td>
-            <img src={boost.image} />
-          </td>
-          <td>{boost.name}</td>
-          <td>{`${boost.exp}%`}</td>
-          <td>{`${boost.sp}%`}</td>
-          <td>{boost.group}</td>
-        </tr>
-      );
-    })}
+  <table className="pure-table">
+    <thead>
+      <tr>
+        <th>image</th>
+        <th>name</th>
+        <th>EXP</th>
+        <th>SP</th>
+        <th>Group</th>
+      </tr>
+    </thead>
+    {groups.map((group, index) => (
+      <Group key={group} group={group as BoostGroup} boosts={byGroup[group]} odd={index % 2 === 1} />
+    ))}
   </table>
 );
 
