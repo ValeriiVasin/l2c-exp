bb15a33366057198fff5673bba004ec88c49072f
Valerii Vasin <vvasin@groupon.com>
2018-11-10 03:40:21 +0100

    implement numbers parser

--- helpers.spec.ts
+++ helpers.spec.ts
@@ -1,4 +1,4 @@
-import { addBoost } from './helpers';
+import { addBoost, toNumber } from './helpers';
 import { BoostId } from './constants';
 
 describe('add boost', () => {
@@ -13,5 +13,33 @@ describe('add boost', () => {
     expect(addBoost([BoostId.FortuneOne], BoostId.FortuneTwo)).toEqual([
       BoostId.FortuneTwo
     ]);
+  });
+});
+
+describe('parsing string to number', () => {
+  it('parses number', () => {
+    expect(toNumber('10')).toBe(10);
+  });
+
+  it('parses number with trailing space', () => {
+    expect(toNumber('10 ')).toBe(10);
+  });
+  it('parses number with spaces in between', () => {
+    expect(toNumber('10 000 000')).toBe(10000000);
+  });
+  it('parses floating number', () => {
+    expect(toNumber('10.2')).toBe(10.2);
+  });
+  it('parses floating number with comma separator', () => {
+    expect(toNumber('10,2')).toBe(10.2);
+  });
+  it('parses k suffix', () => {
+    expect(toNumber('10k')).toBe(10000);
+    expect(toNumber('10kk')).toBe(10000000);
+  });
+  it('parses k suffix with floating number', () => {
+    expect(toNumber('10.2k')).toBe(10200);
+    expect(toNumber('10,2k')).toBe(10200);
+    expect(toNumber('10.2kk')).toBe(10200000);
   });
 });
--- helpers.ts
+++ helpers.ts
@@ -1,8 +1,26 @@
-import { Boost, byId } from './boost';
+import { byId } from './boost';
 import { BoostId } from './constants';
 
 export const addBoost = (ids: BoostId[], id: BoostId): BoostId[] => {
   const group = byId[id].group;
 
   return [...ids.filter(id => byId[id].group !== group), id];
+};
+
+export const toNumber = (value: string): number => {
+  // multiplier for kk numbers
+  const multiplier = Math.pow(
+    10,
+    3 * (value.trim().length - value.trim().replace(/k*$/g, '').length)
+  );
+
+  return (
+    multiplier *
+    Number(
+      value
+        .replace(/\s/g, '')
+        .replace(/,/g, '.')
+        .replace(/k/g, '')
+    )
+  );
 };
