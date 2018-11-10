d6aac727489db226389de3b133f45f031fe593f2
Valerii Vasin <vvasin@groupon.com>
2018-11-10 04:15:59 +0100

    add number formatter

--- helpers.spec.ts
+++ helpers.spec.ts
@@ -1,4 +1,4 @@
-import { addBoost, toNumber } from './helpers';
+import { addBoost, toNumber, formatNumber } from './helpers';
 import { BoostId } from './constants';
 
 describe('add boost', () => {
@@ -24,22 +24,50 @@ describe('parsing string to number', () => {
   it('parses number with trailing space', () => {
     expect(toNumber('10 ')).toBe(10);
   });
+
   it('parses number with spaces in between', () => {
     expect(toNumber('10 000 000')).toBe(10000000);
   });
+
   it('parses floating number', () => {
     expect(toNumber('10.2')).toBe(10.2);
   });
+
   it('parses floating number with comma separator', () => {
     expect(toNumber('10,2')).toBe(10.2);
   });
+
   it('parses k suffix', () => {
     expect(toNumber('10k')).toBe(10000);
     expect(toNumber('10kk')).toBe(10000000);
   });
+
   it('parses k suffix with floating number', () => {
     expect(toNumber('10.2k')).toBe(10200);
     expect(toNumber('10,2k')).toBe(10200);
     expect(toNumber('10.2kk')).toBe(10200000);
+  });
+});
+
+describe('formatting number', () => {
+  it('does not format numbers below 1000', () => {
+    expect(formatNumber(999)).toBe('999');
+  });
+
+  it('splits every 3 digit with the space for numbers above 999', () => {
+    expect(formatNumber(1000)).toBe('1 000');
+    expect(formatNumber(10000)).toBe('10 000');
+  });
+
+  it('appends fractional part without formatting and using the dot as a separator', () => {
+    expect(formatNumber(1000.22)).toBe('1 000.22');
+  });
+
+  it('rounds fractional part to 2 digits', () => {
+    expect(formatNumber(22.3333)).toBe('22.33');
+  });
+
+  it('formats NaN as an empty', () => {
+    expect(formatNumber(NaN)).toBe('');
   });
 });
--- helpers.ts
+++ helpers.ts
@@ -24,3 +24,29 @@ export const toNumber = (value: string): number => {
     )
   );
 };
+
+export const formatNumber = (value: number): string => {
+  if (Number.isNaN(value)) {
+    return '';
+  }
+
+  const decimal = Math.floor(value);
+  const fractional = value - decimal;
+
+  const fractionalString = fractional
+    ? `.${fractional.toFixed(2).slice(-2)}`
+    : '';
+  const decimalString = String(decimal)
+    .split('')
+    .reverse()
+    .reduce((acc, digit, index) => {
+      if (index % 3 === 2) {
+        return ` ${digit}${acc}`;
+      }
+
+      return `${digit}${acc}`;
+    }, '')
+    .trim();
+
+  return `${decimalString}${fractionalString}`;
+};
